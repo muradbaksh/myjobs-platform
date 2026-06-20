74aa0c9d6886c811693d260b674e184cd95506dd
muradbaksh <mdmurad8363@gmail.com>
2026-06-20 16:30:44 +0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.user_profile, name='profile'),
-    path('verify/<uidb64>/<token>/', views.verify_email, name='verify_email'),
+    # path('verify/<uidb64>/<token>/', views.verify_email, name='verify_email'),
     
     path('profile-update/',views.profile_update,name='profile_update'),
     path('password-change/',auth_views.PasswordChangeView.as_view(template_name='password_change.html'),name='password_change'),
